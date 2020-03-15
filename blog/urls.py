d4cd64bfc77bfeaf6f39770d56e5a31420a5fe20
Pablo Buisson <pablo.buisson@gmail.com>
2020-03-15 20:46:09 +0100

    add first templates

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('accueil', views.home),
+    path('date', views.date_actuelle),
+    path('addition/<int:nombre1>/<int:nombre2>/', views.addition)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,10 @@
-from django.http import HttpResponse 
-# la classe HttpResponse permet de retourner une reponse depuis un string
+from datetime import datetime
 from django.shortcuts import render
 
-def home(request): # paramètre request : instance de HttpResponse (contient des infos sur
-    # la méthode de requête, les données, ect.)
-    """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
-    return HttpResponse("""
-        <h1>Bienvenue sur mon blog !</h1>
-        <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>
-    """)
+def date_actuelle(request):
+    return render(request, 'blog/date.html', {'date': datetime.now()})
+
+def addition(request, nombre1, nombre2):    
+    total = nombre1 + nombre2
+    # Retourne nombre1, nombre2 et la somme des deux au tpl
+    return render(request, 'blog/addition.html', locals())
