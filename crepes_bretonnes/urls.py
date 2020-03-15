baf6ab0e6c587dc79222729aee5d12f12ee2436b
Pablo Buisson <pablo.buisson@gmail.com>
2020-03-15 17:16:43 +0100

    add customized url (good practice)

--- crepes_bretonnes/urls.py
+++ crepes_bretonnes/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog import views  # ++
+from django.urls import path, include # include pour lier des urls à un path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accueil', views.home),  # ++
-    # path('', views.home) pour une page d'accueil à la racine
+    path('blog/', include('blog.urls')),
+    # tous les urls de l'application blog commenceront par blog/
 ]
