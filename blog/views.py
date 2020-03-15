60af4e7850dbfa42a32fdc304ba038ad5b8652d7
Pablo Buisson <pablo.buisson@gmail.com>
2020-03-15 17:00:08 +0100

    add first view (experimental)

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse 
+# la classe HttpResponse permet de retourner une reponse depuis un string
 from django.shortcuts import render
 
-# Create your views here.
+def home(request): # paramètre request : instance de HttpResponse (contient des infos sur
+    # la méthode de requête, les données, ect.)
+    """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
+    return HttpResponse("""
+        <h1>Bienvenue sur mon blog !</h1>
+        <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>
+    """)
--- crepes_bretonnes/urls.py
+++ crepes_bretonnes/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views  # ++
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accueil', views.home),  # ++
+    # path('', views.home) pour une page d'accueil à la racine
 ]
