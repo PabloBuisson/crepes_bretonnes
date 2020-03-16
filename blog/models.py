c01d6d6e08cd2955c29abac27ab2edf127ddf44a
Pablo Buisson <pablo.buisson@gmail.com>
2020-03-16 20:01:56 +0100

    add first model, make first migration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Article(models.Model):
+    titre = models.CharField(max_length=100)
+    auteur = models.CharField(max_length=42)
+    contenu = models.TextField(null=True)
+    date = models.DateTimeField(default=timezone.now, 
+                                verbose_name="Date de parution")
+    
+    class Meta:
+        verbose_name = "article"
+        ordering = ['date']
+    
+    def __str__(self):
+        """ 
+        Cette méthode que nous définirons dans tous les modèles
+        nous permettra de reconnaître facilement les différents objets que 
+        nous traiterons plus tard dans l'administration
+        """
+        return self.titre
