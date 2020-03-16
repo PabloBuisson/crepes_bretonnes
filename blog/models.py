bf4918c4a6a3a337e5024b13b49000ab7e5b05d3
Pablo Buisson <pablo.buisson@gmail.com>
2020-03-16 23:10:05 +0100

    add Categorie model (as foreign key)

--- blog/models.py
+++ blog/models.py
@@ -5,17 +5,19 @@ class Article(models.Model):
     titre = models.CharField(max_length=100)
     auteur = models.CharField(max_length=42)
     contenu = models.TextField(null=True)
-    date = models.DateTimeField(default=timezone.now, 
+    date = models.DateTimeField(default=timezone.now,
                                 verbose_name="Date de parution")
-    
+    categorie = models.ForeignKey('Categorie', on_delete=models.CASCADE)  # ++
+
     class Meta:
-        verbose_name = "article"
         ordering = ['date']
-    
+
     def __str__(self):
-        """ 
-        Cette méthode que nous définirons dans tous les modèles
-        nous permettra de reconnaître facilement les différents objets que 
-        nous traiterons plus tard dans l'administration
-        """
         return self.titre
+
+
+class Categorie(models.Model):
+    nom = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nom
