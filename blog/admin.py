f524cfdba09853defc829fdf073f51b27576fe4e
Pablo Buisson <pablo.buisson@gmail.com>
2020-03-19 20:45:31 +0100

    add fieldset to models form

--- blog/admin.py
+++ blog/admin.py
@@ -4,12 +4,29 @@ from django.utils.text import Truncator
 from blog.models import Categorie, Article
 
 class ArticleAdmin(admin.ModelAdmin):
+
+    # Configuration de la liste d'articles
     list_display = ('titre', 'categorie', 'auteur', 'date', 'apercu_contenu')
     list_filter = ('auteur', 'categorie', )
     date_hierarchy = 'date'
     ordering = ('date', )
     search_fields = ('titre', 'contenu')
 
+    # Configuration du formulaire d'édition
+    fieldsets = (
+        # Fieldset 1 : meta-info (titre, auteur…)
+        ('Général', {
+            'classes': ['collapse', ], # rend l'ensemble masqué (à dérouler)
+            'fields': ('titre', 'auteur', 'categorie')
+        }),
+        # Fieldset 2 : contenu de l'article
+        ('Contenu de l\'article', {
+            'description': 'Le formulaire accepte les balises HTML. Utilisez-les à bon escient !',
+            'fields': ('contenu', )
+        }),
+    )
+
+    # Colonnes personnalisées
     def apercu_contenu(self, article):
         """ 
         Retourne les 40 premiers caractères du contenu de l'article, 
