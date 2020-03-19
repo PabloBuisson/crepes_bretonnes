8f1b9c216348f554b96050a16239f6490ce952e6
Pablo Buisson <pablo.buisson@gmail.com>
2020-03-19 20:24:01 +0100

    customize field in admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
-from .models import Categorie, Article
+from django.utils.text import Truncator
+
+from blog.models import Categorie, Article
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('titre', 'auteur', 'date')
-    list_filter = ('auteur', 'categorie',)
+    list_display = ('titre', 'categorie', 'auteur', 'date', 'apercu_contenu')
+    list_filter = ('auteur', 'categorie', )
     date_hierarchy = 'date'
     ordering = ('date', )
     search_fields = ('titre', 'contenu')
+
+    def apercu_contenu(self, article):
+        """ 
+        Retourne les 40 premiers caractères du contenu de l'article, 
+        suivi de points de suspension si le texte est plus long. 
+        """
+        return Truncator(article.contenu).chars(40, truncate='...')
+
+    # En-tête de notre colonne
+    apercu_contenu.short_description = 'Aperçu du contenu'
 
 admin.site.register(Categorie)
 admin.site.register(Article, ArticleAdmin)
