acdde68b1a7aa1c5b026c84a5d7dc4620e079cac
Pablo Buisson <pablo.buisson@gmail.com>
2020-03-19 20:05:21 +0100

    customize admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Categorie, Article
 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'auteur', 'date')
+    list_filter = ('auteur', 'categorie',)
+    date_hierarchy = 'date'
+    ordering = ('date', )
+    search_fields = ('titre', 'contenu')
+
 admin.site.register(Categorie)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
