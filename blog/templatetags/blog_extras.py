761d71a60c401d7511e823620b6935a5281b2d20
Pablo Buisson <pablo.buisson@gmail.com>
2020-03-29 11:42:51 +0200

    add customized filter with argument

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -12,3 +12,43 @@ def citation(texte):
     """
     res = "&laquo; {} &raquo;".format(escape(texte))
     return mark_safe(res)
+
+
+@register.filter
+def smart_truncate(texte, nb_caracteres=20):
+    """
+    Coupe la chaîne de caractères jusqu'au nombre de caractères souhaité,
+    sans couper la nouvelle chaîne au milieu d'un mot.
+    Si la chaîne est plus petite, elle est renvoyée sans points de suspension.
+    ---
+    Exemple d'utilisation :
+    {{ "Bonjour tout le monde, c'est Diego"|smart_truncate:18 }} renvoie
+    "Bonjour tout le..."
+    """
+    # Nous vérifions tout d'abord que l'argument passé est bien un nombre
+    try:
+        nb_caracteres = int(nb_caracteres)
+    except ValueError:
+        return texte  # Retour de la chaîne originale sinon
+
+    # Si la chaîne est plus petite que le nombre de caractères maximum voulus,
+    # nous renvoyons directement la chaîne telle quelle.
+    if len(texte) <= nb_caracteres:
+        return texte
+
+    # Sinon, nous coupons au maximum, tout en gardant le caractère suivant
+    # pour savoir si nous avons coupé à la fin d'un mot ou en plein milieu
+    texte = texte[:nb_caracteres + 1]
+    # slice [:nb] = garde tous les éléments, de 0 à nb
+
+    # Nous vérifions d'abord que le dernier caractère n'est pas une espace,
+    # autrement, il est inutile d'enlever le dernier mot !
+    if texte[-1:] != ' ':
+        mots = texte.split(' ')[:-1]
+        # enlève le dernier mot du texte.split
+        texte = ' '.join(mots)
+    else:
+        texte = texte[0:-1]
+        # garde toutes les lettres sauf la dernière
+
+    return texte + '…'
